b6739118219d8d9253c57bfe7e81fbccdd569919
NekoMimi-Cateye <umakko.lumiere@icloud.com>
2021-09-22 20:37:14 +0900

    write code (Test3) at clock_test.c

--- sec14/clock_test.c
+++ sec14/clock_test.c
@@ -4,22 +4,24 @@
 int main(void)
 {
     // 変数群
-    time_t t1, t2;
-    struct tm *pt1, *pt2;
+    time_t t1;
+    time_t t2;
+    struct tm *pt1;
+    struct tm *pt2;
 
-    // time構造体作成
+    // 開始時刻を t1,pt1 として格納
     time(&t1);
     pt1 = localtime(&t1);
 
     // time構造体の内部確認
     while(1)
     {
-        // 更新テスト
+        // 現在時刻を t2,pt2 として格納
         time(&t2);
         pt2 = localtime(&t2);
 
-        // config
+        // なぜか勝手に t1 も更新されている件の確認
         printf("\rpt1 -> tm_sec :: %d  ", pt1 -> tm_sec);
-        printf("pt2 -> tm_sec :: %d    ",   pt2 -> tm_sec);
+        printf("pt2 -> tm_sec :: %d    ", pt2 -> tm_sec);
     }
 }
