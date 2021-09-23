a3709d2b45c7034b624684b282d29ae4e0126915
NekoMimi-Cateye <umakko.lumiere@icloud.com>
2021-09-23 19:32:15 +0900

    finished porting the console clock (at skeleton)

--- sec14/analog_clock/openwindow.c
+++ sec14/analog_clock/openwindow.c
@@ -1,4 +1,6 @@
 #include<GL/glut.h>
+#include<stdio.h>
+#include<time.h>
 
 
 void Display(void);
@@ -25,6 +27,30 @@ int main(int argc, char **argv)
 //
 void Display(void)
 {
+    /* sec14/clock1.c より移植・改変 */
+    // 変数群
+    time_t curt;
+    struct tm *p_curt;
+
+    // 曜日の定義
+    char l_week[7][4] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+    // 現在時刻の更新
+    time(&curt);
+    p_curt = localtime(&curt);
+
+    // 表示
+    printf("\r%d/%02d/%02d %3s %02d:%02d:%02d",
+        p_curt -> tm_year + 1900,
+        p_curt -> tm_mon + 1,
+        p_curt -> tm_mday,
+        l_week[p_curt -> tm_wday],
+        p_curt -> tm_hour,
+        p_curt -> tm_min,
+        p_curt -> tm_sec);
+    /* ここまで */
+
     glClear(GL_COLOR_BUFFER_BIT);
     glFlush();
+
 }
